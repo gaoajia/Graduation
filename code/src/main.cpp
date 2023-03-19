91a6d154aec38b4c92b339251dd228ea51fbd0de
gaoajia <gaoajia@qq.com>
2023-03-19 19:17:15 +0800

    update

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,9 +1,82 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <VL6180X_WE.h>
+#include <U8g2lib.h>
+
+#define VL6180X_ADDRESS 0x29
+#define OLED_ADDRESS 0x3C
+
+VL6180xIdentification identification;
+VL6180x sensor(VL6180X_ADDRESS);
+
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);
+
+void printIdentification(struct VL6180xIdentification *temp){
+  Serial.print("设备 ID = ");
+  Serial.println(temp->idModel);
+
+  Serial.print("设备 Rev = ");
+  Serial.print(temp->idModelRevMajor);
+  Serial.print(".");
+  Serial.println(temp->idModelRevMinor);
+
+  Serial.print("修订 Rev = ");
+  Serial.print(temp->idModuleRevMajor);
+  Serial.print(".");
+  Serial.println(temp->idModuleRevMinor);  
+
+  Serial.print("生产日期 = ");
+  Serial.print((temp->idDate >> 3) & 0x001F);
+  Serial.print("/");
+  Serial.print((temp->idDate >> 8) & 0x000F);
+  Serial.print("/1");
+  Serial.print((temp->idDate >> 12) & 0x000F);
+  Serial.print(" 阶段: ");
+  Serial.println(temp->idDate & 0x0007);
+
+  Serial.print("生产时间 (s)= ");
+  Serial.println(temp->idTime * 2);
+  Serial.println();
+  Serial.println();
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600); 
+  Wire.begin();
+
+  u8g2.begin();
+  u8g2.enableUTF8Print();
+
+  delay(1000); 
+
+  sensor.getIdentification(&identification); 
+  printIdentification(&identification); 
+
+  if(sensor.VL6180xInit() != 0){
+    Serial.println("VL6180初始化失败"); 
+  }; 
+
+  sensor.VL6180xDefautSettings();   
+  delay(1000); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_wqy14_t_gb2312b); 
+  u8g2.setFontDirection(0);
+
+  u8g2.setCursor(0, 15);
+  u8g2.print("光照：");
+  u8g2.print(sensor.getAmbientLight(GAIN_10));
+    u8g2.print(" Lux");
+  u8g2.setCursor(0, 30);
+  u8g2.print("距离：");
+  u8g2.print(sensor.getDistance());
+  u8g2.print(" mm");
+  u8g2.sendBuffer();
+  Serial.print("环境光级别 (Lux) = ");
+  Serial.println(sensor.getAmbientLight(GAIN_10));
+  Serial.print("测量距离 (mm) = ");
+  Serial.println(sensor.getDistance()); 
+  delay(50);  
+};
